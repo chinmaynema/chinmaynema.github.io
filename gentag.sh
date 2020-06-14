0bc7388c0bb0398a1803cb8e1892cd01aad3e083
Chinmay Nema <chinmay.nema1993@gmail.com>
2020-06-14 14:19:38 +0530

    fix gentag

--- gentag.sh
+++ gentag.sh
@@ -2,7 +2,8 @@ git pull -r
 bundle exec jekyll serve -B
 rm -rf tag
 cp -r _site/tag .
-find . -name \*.html -exec sed -i '' -e "s/localhost:4000/onetriptoanother.com/g" {} \;
+find tag/ -name \*.html -exec sed -i '' -e "s/localhost:4000/onetriptoanother.com/g" {} \;
 git add .
 git commit -m "update tag"
 git push
+pkill -f jekyll
